51841f6497cc931afcacd57aa601391e096c1ab4
Adrian Biedrzycki <a@modules.io>
2016-01-11 00:35:08 +0000

    Add basic sync test

--- test/test.js
+++ test/test.js
@@ -56,4 +56,29 @@ describe('sync', () => {
     expect(result).to.be.null;
     expect(Immutable.is(sf.get(), new Immutable.Map())).to.be.true;
   });
+
+  it('should sync basic change', () => {
+    const mf = new MasterFile();
+    expect(Immutable.is(mf.get(), new Immutable.Map())).to.be.true;
+
+    const sf = new SlaveFile();
+    const syncReq = sf.sync();
+    syncReq.accept(mf.apply(syncReq));
+    expect(Immutable.is(sf.get(), new Immutable.Map())).to.be.true;
+
+    const updateReq = sf.update(oldState => {
+      expect(Immutable.is(sf.get(), new Immutable.Map())).to.be.true;
+
+      return oldState.set('lol', 'woot');
+	});
+    expect(updateReq).to.be.a(Patch);
+    expect(sf.get('lol')).to.equal('woot');
+
+    const updateRes = mf.apply(Patch.fromJSON(updateReq.toJSON()));
+    expect(updateRes).to.be.null;
+    expect(mf.get('lol')).to.equal('woot');
+
+    updateReq.accept(updateRes);
+    expect(sf.get('lol')).to.equal('woot');
+  });
 });
